9c5711a246cef25f4a312d1c157822c444a84631
mkrivic83 <krivic.milan@gmail.com>
2026-02-08 21:41:27 +0100

    dodano nesto jos za proxy propust

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Pagination\Paginator;
+
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         Paginator::useBootstrapFive();
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
